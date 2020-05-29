78030209cfe55f14cb486de678504d126fc95e3c
郑柯凡 <2097306716@qq.com>
2020-05-29 17:07:48 +0800

    no message

---
+++ Classes/Props/Props.cpp
@@ -0,0 +1,33 @@
+#include"Props.h"
+
+USING_NS_CC;
+//道具生成
+Props::Props(const char* pngName) {
+	//在玩家位置创建道具
+	_sprite = Sprite::create("pngName.png");
+	_sprite->setPosition(_player->getPosition());
+	
+	//添加道具的物理模型
+	auto physicsBody = PhysicsBody::createBox(_sprite->getContentSize(), PhysicsMaterial(0.0f, 0.0f, 0.0f));
+	physicsBody->setDynamic(false);
+	physicsBody->setContactTestBitmask(0xFFFFFFFF);
+	_sprite->setPhysicsBody(physicsBody);
+
+};
+
+Props::Props(const char* pngName, Node* Box) {
+	//在玩家位置创建道具（可改为在宝箱位置）
+	_sprite = Sprite::create("pngName.png");
+	_sprite->setPosition(Box->getPosition());
+
+	//添加道具的物理模型
+	auto physicsBody = PhysicsBody::createBox(_sprite->getContentSize(), PhysicsMaterial(0.0f, 0.0f, 0.0f));
+	physicsBody->setDynamic(false);
+	physicsBody->setContactTestBitmask(0xFFFFFFFF);
+	_sprite->setPhysicsBody(physicsBody);
+
+};;
+//碰撞检测
+bool Props::onContactWithHero(cocos2d::Node* Hero) {};
+
+
--- Classes/Props/Props.h
+++ Classes/Props/Props.h
@@ -1,1 +1,20 @@
 /* Props.h */
+#ifndef PROPS_H
+#define PROPS_H
+
+#include"cocos2d.h"
+//道具基类
+class Props {
+protected:
+	cocos2d::Sprite* _sprite;
+public:
+	//道具构造函数
+	//1.默认位置Hero处
+	Props(const char* pngName);
+	//2.指定位置处
+	Props(const char* pngName, Node* Box);
+	//是否与英雄碰撞
+	bool onContactWithHero(cocos2d::Node* Hero);
+};
+
+#endif
