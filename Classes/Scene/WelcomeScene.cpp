c87b5eb5e897d405cf21a7f0a5cefc8ac560d556
DorotheaIN <2577672771@qq.com>
2020-06-16 23:49:53 +0800

    adjust the position of musicItem

--- Classes/Scene/WelcomeScene.cpp
+++ Classes/Scene/WelcomeScene.cpp
@@ -1,109 +1,99 @@
 
 #include "WelcomeScene.h"
-//#include "SaveMapScene.h"
-#include "FightGroundScene.h"
+#include "SaveMapScene.h"
 #include "cocos2d.h"
-#include "SimpleAudioEngine.h"
-//#include"..\cocos\editor-support\cocostudio\SimpleAudioEngine.h"
-
-
-
+#include"..\cocos\editor-support\cocostudio\SimpleAudioEngine.h"
 USING_NS_CC;
 
 Scene* Welcome::createScene()
 {
-    return Welcome::create();
+	return Welcome::create();
 }
 
 static void problemLoading(const char* filename)
 {
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+	printf("Error while loading: %s\n", filename);
+	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
 }
 
 bool Welcome::init()
 {
- 
-    if ( !Scene::init() )
-    {
-        return false;
-    }
+
+	if (!Scene::init())
+	{
+		return false;
+	}
 
 	/* ÉèÖÃ±³¾°ÒôÀÖ */
 	auto audio = CocosDenshion::SimpleAudioEngine::getInstance();
 	if (!audio->isBackgroundMusicPlaying()) {
-		audio->playBackgroundMusic("Soulknightbgm.mp3", true);
+		audio->playBackgroundMusic("SoulKnightBgm.mp3", true);
 	}
-    isMusicPlaying = true;
 
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
 	/* ÒôÀÖ¿ØÖÆ°´Å¥ */
-	auto offMusic = MenuItemImage::create("MusicSelected.png", "MusicSelected.png");
-	auto onMusic = MenuItemImage::create("MusicNormal.png", "MusicNormal.png");
-    //add
-    offMusic->setScale(0.5);
-    onMusic->setScale(0.5);
-    
+	MenuItemImage *offMusic = MenuItemImage::create("MusicSelected.png", "MusicSelected.png");
+	MenuItemImage *onMusic = MenuItemImage::create("MusicNormal.png", "MusicNormal.png");
+	offMusic->setScale(0.5);
+	onMusic->setScale(0.5);
+
 	MenuItemToggle *musicItem = MenuItemToggle::createWithCallback(
 		CC_CALLBACK_1(Welcome::menuCloseMusic, this),
 		onMusic, offMusic, NULL
 	);
-	musicItem->setPosition(Vec2(130, visibleSize.height / 2-300));
+	musicItem->setPosition(Vec2(100, visibleSize.height / 2 - 230));
 
 	/* ½øÈëÓÎÏ·°´Å¥ */
-	auto startItem = MenuItemImage::create("NewGameIcon.png", "NewGameIcon.png", CC_CALLBACK_1(Welcome::menuStartCallBack, this));
+	MenuItemImage *startItem = MenuItemImage::create("NewGameIcon.png", "NewGameIcon.png", CC_CALLBACK_1(Welcome::menuStartCallBack, this));
 	startItem->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 3 - 120));
-    //add
-    startItem->setScale(0.5);
-    
-	/* ²Ëµ¥ */
-    auto menu = Menu::create(musicItem,startItem,NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
+	startItem->setScale(0.5);
 
-	
-    auto sprite = Sprite::create("CoverSelected.png");
-    //add
-    auto scaleX = visibleSize.width / sprite->getContentSize().width;
-    auto scaleY = visibleSize.height / sprite->getContentSize().height;
-    auto realScale = scaleX > scaleY ? scaleX : scaleY;
-    sprite->setScale(realScale);
-    
-    if (sprite == nullptr)
-    {
-        problemLoading("CoverSelected.png");
-    }
-    else
-    {
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-        this->addChild(sprite, 0);
-    }
-    return true;
+	/* ²Ëµ¥ */
+	Menu *menu = Menu::create(musicItem, startItem, NULL);
+	menu->setPosition(Vec2::ZERO);
+	this->addChild(menu, 1);
+
+	/* ±³¾°Í¼Æ¬ */
+	Sprite *background = Sprite::create("CoverSelected.png");
+	auto scaleX = visibleSize.width / background->getContentSize().width;
+	auto scaleY = visibleSize.height / background->getContentSize().height;
+	auto realScale = scaleX > scaleY ? scaleX : scaleY;
+	background->setScale(realScale);
+
+	if (background == nullptr)
+	{
+		problemLoading("CoverSelected.png");
+	}
+	else
+	{
+		background->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+		this->addChild(background, 0);
+	}
+	return true;
 }
 
 
 void Welcome::menuCloseCallback(Ref* pSender)
 {
-    Director::getInstance()->end();
+	Director::getInstance()->end();
 }
 
 void Welcome::menuStartCallBack(Ref* pSender)
 {
-	auto nextScene = FightGround::createScene();
+	auto nextScene = SaveMap::createScene();
 	Director::getInstance()->replaceScene(nextScene);
 }
 
 void Welcome::menuCloseMusic(Ref* pSender)
 {
 	auto audio = CocosDenshion::SimpleAudioEngine::getInstance();
-	if (isMusicPlaying == true) {
+	if (audio->isBackgroundMusicPlaying()) {
 		audio->pauseBackgroundMusic();
-        isMusicPlaying = false;
 	}
 	else {
 		audio->resumeBackgroundMusic();
-        isMusicPlaying = true;
 	}
 }
