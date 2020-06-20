69594345cd5cc41e9e3b319d6c023627f50fefa3
Wizard23333 <142364>
2020-06-20 18:06:17 +0800

    增加boss场景

--- Classes/Props/Props.h
+++ Classes/Props/Props.h
@@ -32,7 +32,7 @@ public:
 const std::vector<Propsvalue> parameter = {
 	Propsvalue("Blood.png",2,0,0,0),Propsvalue("Energy.png",0,20,0,0),Propsvalue("HelloWorld.png",1,1,0,0),Propsvalue("HelloWorld.png",1,1,0,0),Propsvalue("HelloWorld.png",1,1,0,0),
 	Propsvalue("Sickle.png",1,1,1,1),Propsvalue("Sword.png",1,1,1,1),Propsvalue("Darts.png",1,1,1,1),Propsvalue("Gun.png",1,1,1,1),Propsvalue("HelloWorld.png",1,1,1,1),
-	Propsvalue("Sickle.png",4,1,1,1),Propsvalue("Sword.png",3,0,1,1),Propsvalue("Darts.png",1,1,1,1),Propsvalue("Bullet.png",2,1,1,1),Propsvalue("HelloWorld.png",1,1,1,1),
+	Propsvalue("Sickle.png",4,1,1,1),Propsvalue("Sword.png",3,0,1,1),Propsvalue("Darts.png",1,1,1,1),Propsvalue("Bullet.png",2,0,1,1),Propsvalue("HelloWorld.png",1,1,1,1),
 	Propsvalue("HelloWorld.png",1,1,1,1),Propsvalue("HelloWorld.png",1,1,1,1),Propsvalue("HelloWorld.png",1,1,1,1),Propsvalue("HelloWorld.png",1,1,1,1),Propsvalue("HelloWorld.png",1,1,1,1),
 };
 /*const Propsvalue parameter[20] = {
---
+++ Classes/Scene/bossScene.h
@@ -0,0 +1,62 @@
+
+#ifndef bossScene_h
+#define bossScene_h
+#include "HelloWorldScene.h"
+#include "PauseScene.h"
+#include "WelcomeScene.h"
+#include "FightGroundScene.h"
+#include "Scene3.h"
+#include "cocos2d.h"
+#include "Hero.h"
+#include "Monster.h"
+#include "Potion.h"
+#include "Box.h"
+#include "Wall.h"
+USING_NS_CC;
+class bossScene : public cocos2d::Scene
+{
+public:
+    static cocos2d::Scene * createScene(int blood = 100, int energy = 50, int shield = 30);
+    virtual bool init();
+    void menucloseCallBack(cocos2d::Ref * pSender);
+    void menucloseMusic(cocos2d::Ref * pSender);
+    void nextScene();
+    bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+    bool onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+    bool onContactBegan(cocos2d::PhysicsContact &);
+    bool onKeyPressed(cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event * event);
+    bool onKeyReleased(cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event * event);
+    CREATE_FUNC(bossScene);
+private:
+    void monsterinit();//*****
+    void addmonster(float dt);//*****
+    void automoveM(float dt);//*****
+    void autoshootM(float dt);//****
+    void updateBlood(float dt);
+    void valueAdd(float dt);
+    void controlMoveArea(float dt);
+    void controlSprite(Sprite * sprite);
+    void controlMovingActor(PhysicsContact & contact);
+    void appearSprite();
+    bool isMusicPlaying;
+    Size visibleSize;
+    Vec2 originPoint;
+    Label * blood;
+    Label * energy;
+    Label * sheild;
+    static int s3_blood;
+    static int s3_energy;
+    static int s3_shield;
+    int appearTime;
+    Hero myHero;
+    Potion potion1;
+    Potion potion2;
+    Props gate;
+    Box box1;
+    Monster boss;
+    int shootAngle;
+    
+    
+};
+
+#endif /* bossScene_h */
