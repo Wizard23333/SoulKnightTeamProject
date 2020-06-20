da3bb97e9b19af9e6c99ef50ee64ed145db52bc4
DorotheaIN <2577672771@qq.com>
2020-06-21 00:33:47 +0800

    安全地图成功实现碰撞

--- Classes/Scene/SaveMapScene.h
+++ Classes/Scene/SaveMapScene.h
@@ -5,32 +5,34 @@
 #include "cocos2d.h"
 #include <map>
 #include "Hero.h"
+#include "FightGroundScene.h"
 
 
 class SaveMap:public cocos2d::Scene
 {
 public:
-	static cocos2d::Scene* createScene();
-	virtual bool init();
-	void menuCloseCallback(cocos2d::Ref* pSender);
-	bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* unused_event);
-	bool onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* unused_event);
-	bool onContactBegan(cocos2d::PhysicsContact &);
-	bool onKeyPressed(cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event * event);
-	bool onKeyReleased(cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event * event);
-	CREATE_FUNC(SaveMap);
-	void mapUpdate(Point kinght);
+    static cocos2d::Scene* createScene();
+    virtual bool init();
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+    bool onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+    bool onContactBegan(cocos2d::PhysicsContact &);
+    bool onKeyPressed(cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event * event);
+    bool onKeyReleased(cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event * event);
+    CREATE_FUNC(SaveMap);
+    void mapUpdate(Point kinght);
 
 private:
-	Hero myHero;
-	void setMapPosForView(Point playerPos);
+    Hero myHero;
+    void setMapPosForView(Point playerPos);
     void update(float delta);
-	void updateHeroAction();
-	void updateMap();
-	TMXTiledMap *tileMap;
-	Point cocoscoord2tilemapcoord(Point pos);
-	void isCollide();
-	
+    void updateHeroAction();
+    void updateMap();
+    TMXTiledMap *tileMap;
+    Point cocoscoord2tilemapcoord(Point pos);
+    void isCollide();
+    void nextScene(float dt);
+    
 
 };
 
