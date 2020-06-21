5435c8a70208032f5ced039c81cd9e95b897634c
DorotheaIN <2577672771@qq.com>
2020-06-21 13:10:42 +0800

    for merge

--- Classes/Scene/WelcomeScene.cpp
+++ Classes/Scene/WelcomeScene.cpp
@@ -3,6 +3,8 @@
 #include "SaveMapScene.h"
 #include "cocos2d.h"
 #include  "C:\Users\Dell\Desktop\cocos2d-x-3.17.2\cocos2d-x-3.17.2\cocos\audio\include\SimpleAudioEngine.h"
+#include "SimpleAudioEngine.h"
+#include "SaveMapScene.h"
 
 USING_NS_CC;
 
@@ -85,8 +87,8 @@ void Welcome::menuCloseCallback(Ref* pSender)
 
 void Welcome::menuStartCallBack(Ref* pSender)
 {
+
     auto nextScene = SaveMap::createScene();
-    
 	Director::getInstance()->replaceScene(nextScene);
 }
 
